42fa01994da1cad6a3ce2e23b52860511d6c6794
yonatanB2013 <yonatan.bard@gmail.com>
2026-06-24 17:08:59 +0300

    Prepare for Render deploy

--- main.py
+++ main.py
@@ -79,7 +79,6 @@ def results():
     conn.close()
     return render_template('results.html', data=data)
 
-
+init_db()
 if __name__ == '__main__':
-    init_db()
     app.run(debug=True, port = 5001)
